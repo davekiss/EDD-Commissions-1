f7b5c929745e2213f62cdca1957a472717f20035
Sunny Ratilal <sunny@sunnyratilal.com>
2017-02-14 22:12:50 +0000

    Add WP_Post attributes and constructor #133

--- includes/class-edd-commission.php
+++ includes/class-edd-commission.php
@@ -89,4 +89,68 @@ class EDD_Commission {
 	 * @var int
 	 */
 	protected $payment_ID = 0;
+
+	/**
+	 * Array of items that have changed since the last save() was run.
+	 * This is for internal use, to allow fewer update_post_meta calls to be run.
+	 *
+	 * @since 3.3
+	 * @access private
+	 * @var array
+	 */
+	private $pending;
+
+	/**
+	 * Declare the default properties in WP_Post as we can't extend it.
+	 *
+	 * @since 3.3
+	 * @access protected
+	 * @var mixed
+	 */
+	protected $post_author = 0;
+	protected $post_date = '0000-00-00 00:00:00';
+	protected $post_date_gmt = '0000-00-00 00:00:00';
+	protected $post_content = '';
+	protected $post_title = '';
+	protected $post_excerpt = '';
+	protected $post_status = 'publish';
+	protected $comment_status = 'open';
+	protected $ping_status = 'open';
+	protected $post_password = '';
+	protected $post_name = '';
+	protected $to_ping = '';
+	protected $pinged = '';
+	protected $post_modified = '0000-00-00 00:00:00';
+	protected $post_modified_gmt = '0000-00-00 00:00:00';
+	protected $post_content_filtered = '';
+	protected $post_parent = 0;
+	protected $guid = '';
+	protected $menu_order = 0;
+	protected $post_mime_type = '';
+	protected $comment_count = 0;
+	protected $filter;
+	protected $post_type;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 3.3
+	 * @access protected
+	 *
+	 * @param int $id Commission ID.
+	 */
+	public function __construct( $id = false ) {
+		if ( empty( $id ) ) {
+			return false;
+		}
+
+		$id = absint( $id );
+		$commission = WP_Post::get_instance( $id );
+
+		if ( $commission ) {
+			$this->setup_commission( $commission );
+		} else {
+			return false;
+		}
+	}
 }
