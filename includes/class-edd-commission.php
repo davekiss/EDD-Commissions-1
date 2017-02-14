65ac17bb630eed40cb2c0a293f122ec216ff8562
Sunny Ratilal <sunny@sunnyratilal.com>
2017-02-14 22:07:31 +0000

    Add class variables #133

--- includes/class-edd-commission.php
+++ includes/class-edd-commission.php
@@ -18,5 +18,75 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * @since 3.3
  */
 class EDD_Commission {
+	/**
+	 * Commission ID.
+	 *
+	 * @since 3.3
+	 * @access protected
+	 * @var int
+	 */
+	protected $ID = 0;
 
+	/**
+	 * User ID.
+	 *
+	 * @since 3.3
+	 * @access protected
+	 * @var int
+	 */
+	protected $user_ID = 0;
+
+	/**
+	 * Commission Rate.
+	 *
+	 * @since 3.3
+	 * @access protected
+	 * @var mixed float|int
+	 */
+	protected $rate;
+
+	/**
+	 * Commission Amount.
+	 *
+	 * @since 3.3
+	 * @access protected
+	 * @var mixed float|int
+	 */
+	protected $amount;
+
+	/**
+	 * Currency.
+	 *
+	 * @since 3.3
+	 * @access protected
+	 * @var string
+	 */
+	protected $currency;
+
+	/**
+	 * Download ID.
+	 *
+	 * @since 3.3
+	 * @access protected
+	 * @var int
+	 */
+	protected $download_ID = 0;
+
+	/**
+	 * User ID.
+	 *
+	 * @since 3.3
+	 * @access protected
+	 * @var int
+	 */
+	protected $user_ID = 0;
+
+	/**
+	 * Payment ID.
+	 *
+	 * @since 3.3
+	 * @access protected
+	 * @var int
+	 */
+	protected $payment_ID = 0;
 }
