30a8e3137c030a39794ad18ad23bd54b9d4cc32a
Sunny Ratilal <sunny@sunnyratilal.com>
2017-02-14 17:48:51 +0000

    Add shell for EDD_Commission class #133

---
+++ includes/class-edd-commission.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Commission Object
+ *
+ * @package     Easy Digital Downloads - Commissions
+ * @subpackage  Classes/Discount
+ * @copyright   Copyright (c) 2017, Sunny Ratilal
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       3.3
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * EDD_Commission Class
+ *
+ * @since 3.3
+ */
+class EDD_Commission {
+
+}
